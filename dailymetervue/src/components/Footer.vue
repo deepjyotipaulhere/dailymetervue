bc7f988880c7a2ecd00ce25286cc820901603843
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2018-05-08 18:01:15 +0530

    two

---
+++ dailymetervue/src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+            <footer class="page-footer">
+                <div class="container">
+                    <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">Follow Us</h5>
+                        <p class="grey-text text-lighten-4">Follow us in Twitter</p>
+                    </div>
+                    <div class="col l4 offset-l2 s12">
+                        <h5 class="white-text">Links</h5>
+                        <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <div class="container">
+                        <h6 class="grey-text text-darken-3 center" href="#!" style="font-family:'Zilla'">&copy; Dailymeter</h6>
+                    </div>
+                </div>
+            </footer>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- dailymetervue/src/components/Login.vue
+++ dailymetervue/src/components/Login.vue
@@ -2,39 +2,82 @@
     <div>
         <div style="background:url('/static/image/home.jpg');background-size:cover;min-height:80vh;background-position:center">
             <div class="container" style="padding-top:30px;">
-                <h2 style="margin:0;color:white">Welcome to Dailymeter</h2>
+                <h2 style="margin:0;color:white">Dailymeter</h2>
                 <p style="margin:0;color:white">Forever Memories</p>
             </div>
         </div>
         <div style="padding-top:30px;">
             <div class="container">
                 <div class="row">
-                <div class="card black">
-                    <div class="card-tabs">
-                        <ul class="tabs tabs-fixed-width">
-                            <li class="tab"><a href="#test4">Log In</a></li>
-                            <li class="tab"><a href="#test6">Sign Up</a></li>
-                        </ul>
-                    </div>
-                    <div class="card-content grey lighten-4">
-                        <div id="test4">
-                            <form>
-                                <div class="input-field col s12">
-                                    <input id="email" type="email" style="font-family:'Zilla'">
-                                    <label for="email">Email</label>
+                    <div class="col s6 m6 l6">
+                        <div class="card">
+                            <div class="card-tabs">
+                                <ul class="tabs tabs-fixed-width">
+                                    <li class="tab"><a href="#login">Log In</a></li>
+                                    <li class="tab"><a href="#signup">Register</a></li>
+                                </ul>
+                            </div>
+                            <div class="card-content white">
+                                <div id="login">
+                                    <form>
+                                        <div class="input-field col s12">
+                                            <input id="email" type="email" style="font-family:'Zilla'" v-model="logindata.email">
+                                            <label for="email">Email</label>
+                                        </div>
+                                        <div class="input-field col s12">
+                                            <input id="pwd" type="password" v-model="logindata.pwd">
+                                            <label for="pwd">Password</label>
+                                        </div>
+                                        <br>
+                                        <button class="btn btn-large black waves-effect waves-light">Sign In</button>
+                                    </form>
                                 </div>
-                                <div class="input-field col s12">
-                                    <input id="pwd" type="password">
-                                    <label for="pwd">Password</label>
+                                <div id="signup">
+                                    <form>
+                                        <div class="input-field col s12">
+                                            <input id="semail" type="email" style="font-family:'Zilla'" v-model="registerdata.email">
+                                            <label for="semail">Email</label>
+                                        </div>
+                                        <div class="input-field col s12">
+                                            <input id="spwd" type="password" v-model="registerdata.pwd">
+                                            <label for="spwd">Password</label>
+                                        </div>
+                                        <div class="input-field col s12">
+                                            <input id="cpwd" type="password">
+                                            <label for="cpwd">Confirm Password</label>
+                                        </div>
+                                        <div class="input-field col s12">
+                                            <input id="cname" type="text" v-model="registerdata.name">
+                                            <label for="cname">Your Full Name</label>
+                                        </div>
+                                        <br>
+                                        <button class="btn btn-large black waves-effect waves-light">Register</button>
+                                    </form>
                                 </div>
-                                <br>
-                                <button class="btn btn-large black waves-effect waves-light">Sign In</button>
-                            </form>
+                            </div>
                         </div>
-                        <div id="test6">Test 3</div>
+                    </div>
+                    <div class="col s6 m6 l6">
+                        <h3>Diary Writing. Simplified.</h3>
+                        <p>Title</p>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div style="background:url('/static/image/home1.jpg');background-size:cover">
+            <div class="container">
+                <div class="row">
+                    <div class="col s6 m6 l6" style="color:white">
+                        <h2>Your Story</h2>
+                        <p style="font-size:1.1em">Write your daily stories and store it in the cloud. Dailymeter can be your personal diary where you can write about moments, attach photos, audio and videos. Recreate your day digitally.</p>
+                        <h2>Share Moments</h2>
+                        <p style="font-size:1.1em">Share your one memorable day or the entire diary with friends in the popular social networks.</p>
+                        <h2>Get a print</h2>
+                        <p style="font-size:1.1em">You can order a printed copy of your diary and in a quality package at a resonable cost. That includes a disc containing your digital media.</p>
+                    </div>
                 </div>
+                <br>
             </div>
         </div>
     </div>
@@ -44,18 +87,24 @@
 export default {
     data(){
         return {
-
+            logindata:{
+                email:'',
+                pwd:''
+            },
+            registerdata:{
+                email:'',
+                pwd:'',
+                name:''
+            }
         }
     },
     methods:{
-        create(){
-            $(document).ready(function(){
-                $('.tabs').tabs();
-            });
-        }
+        
     },
     created(){
-            this.create();
+        $(document).ready(function(){
+            $('.tabs').tabs();
+        });
     }
 }
 </script>
